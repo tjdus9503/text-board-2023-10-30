d73f3a2887f3f5c27a35f582f0cc6e80d346890d
tjdus9503 <tjdus03095@gmail.com>
2023-11-05 02:24:30 +0900

    TestUtil.setOutToByteArray

--- src/test/java/com/ll/TestUtilTest.java
+++ src/test/java/com/ll/TestUtilTest.java
@@ -3,6 +3,7 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,5 +24,22 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
         assertThat(authorName).isEqualTo("이순신");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray() 테스트")
+    void t2() {
+        // 모니터로 출력을 끄는 것
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        String out = byteArrayOutputStream.toString().trim(); //개행 제거를 위해 사용
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        // 모니터로 출력을 다시 켜는 것
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+        System.out.println("이제는 화면에 출력됩니다.");
     }
 }
